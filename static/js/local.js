a955592a4fc91e6b912f0fb38de8270cad22be47
Eric Meyer <eric@oddbird.net>
2011-12-02 13:53:17 -0700

    wireframe (medium media only)

--- static/js/local.js
+++ static/js/local.js
@@ -1,6 +1,13 @@
+function showbugs() {
+    $('.weekview .weektitle').click(function() {
+        $(this).parent().toggleClass('open').toggleClass('closed');
+    });
+}
+
 $(function() {
     $('#hcard-client-name .email').defuscate();
     $('input[placeholder], textarea[placeholder]').placeholder();
     $('.details:not(html)').html5accordion('.summary');
     $('#messages').messages({handleAjax: true});
+    showbugs();
 });
