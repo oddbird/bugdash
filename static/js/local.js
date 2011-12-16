9b530f35be4c3652c3185445b589d8abe7d50125
Eric Meyer <eric@oddbird.net>
2011-12-15 23:18:43 -0700

    show/hide bugs

--- static/js/local.js
+++ static/js/local.js
@@ -2,6 +2,16 @@ function showbugs() {
     $('.weekview .weektitle').click(function() {
         $(this).parent().toggleClass('open').toggleClass('closed');
     });
+
+    $('.bugtoggle').click(function() {
+        $(this).toggleClass('open');
+        if ($(this).hasClass('open')) {
+          $('.weekview .week, .dayview .day').addClass('open').removeClass('closed');
+        } else {
+          $('.weekview .week, .dayview .day').removeClass('open').addClass('closed');
+        }
+        return false;
+    });
 }
 
 $(function() {
