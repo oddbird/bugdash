9b1aad06bb4bfeb3b67a20dbc1f17c460ed1fa1c
Eric Meyer <eric@oddbird.net>
2011-12-15 23:30:32 -0700

    view toggle works

--- static/js/local.js
+++ static/js/local.js
@@ -1,15 +1,34 @@
-function showbugs() {
+function showBugs() {
     $('.weekview .weektitle').click(function() {
+        $(this).parent().toggleClass('open').toggleClass('closed');
+    });
+
+    $('.dayview .daytitle').click(function() {
         $(this).parent().toggleClass('open').toggleClass('closed');
     });
 
     $('.bugtoggle').click(function() {
         $(this).toggleClass('open');
         if ($(this).hasClass('open')) {
-          $('.weekview .week, .dayview .day').addClass('open').removeClass('closed');
+            $('.weekview .week, .dayview .day').addClass('open').removeClass('closed');
         } else {
-          $('.weekview .week, .dayview .day').removeClass('open').addClass('closed');
+            $('.weekview .week, .dayview .day').removeClass('open').addClass('closed');
         }
+        return false;
+    });
+}
+
+function viewToggle() {
+    $('.showweek').click(function() {
+        $(this).addClass('active');
+        $('.showday').removeClass('active');
+        $('body').removeClass('dayview').addClass('weekview');
+        return false;
+    });
+    $('.showday').click(function() {
+        $(this).addClass('active');
+        $('.showweek').removeClass('active');
+        $('body').removeClass('weekview').addClass('dayview');
         return false;
     });
 }
@@ -19,5 +38,6 @@ $(function() {
     $('input[placeholder], textarea[placeholder]').placeholder();
     $('.details:not(html)').html5accordion('.summary');
     $('#messages').messages({handleAjax: true});
-    showbugs();
+    showBugs();
+    viewToggle();
 });
