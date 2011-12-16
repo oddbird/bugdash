fc248335ba129291ce35b254c591a5a638d7c5e2
Eric Meyer <eric@oddbird.net>
2011-12-15 23:56:52 -0700

    working day view

--- static/js/local.js
+++ static/js/local.js
@@ -6,13 +6,17 @@ function showBugs() {
     $('.dayview .daytitle').click(function() {
         $(this).parent().toggleClass('open').toggleClass('closed');
     });
+}
 
+function bugToggle() {
     $('.bugtoggle').click(function() {
         $(this).toggleClass('open');
         if ($(this).hasClass('open')) {
             $('.weekview .week, .dayview .day').addClass('open').removeClass('closed');
+            $(this).text('« hide bugs');
         } else {
             $('.weekview .week, .dayview .day').removeClass('open').addClass('closed');
+            $(this).text('show bugs »');
         }
         return false;
     });
@@ -23,12 +27,14 @@ function viewToggle() {
         $(this).addClass('active');
         $('.showday').removeClass('active');
         $('body').removeClass('dayview').addClass('weekview');
+        showBugs();
         return false;
     });
     $('.showday').click(function() {
         $(this).addClass('active');
         $('.showweek').removeClass('active');
         $('body').removeClass('weekview').addClass('dayview');
+        showBugs();
         return false;
     });
 }
@@ -39,5 +45,6 @@ $(function() {
     $('.details:not(html)').html5accordion('.summary');
     $('#messages').messages({handleAjax: true});
     showBugs();
+    bugToggle();
     viewToggle();
 });
